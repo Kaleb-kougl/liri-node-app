99f207fb2d5902469686204f5c7cd2947d9f8823
Kaleb Kougl <kalebkougl@gmail.com>
2018-12-08 16:24:56 -0600

    adding param for recursion, adding movie this, adding error hand song

--- liri.js
+++ liri.js
@@ -10,20 +10,29 @@ const BANDS_URL = "https://rest.bandsintown.com/artists/";
 
 var choice = process.argv[2];
 
-function search(userChoice=choice) {
+function search(userChoice=choice, searchParam=undefined) {
   switch (userChoice) {
     case "concert-this":
-      if (process.argv.length > 3) {
-        let band = process.argv[3];
-        for (let i = 4; i < process.argv.length; i++) {
-          band += ` ${process.argv[i]}`
+      if (process.argv.length > 3 || searchParam != undefined) {
+        let band;
+        if (searchParam === undefined) {
+          band = process.argv[3];
+          for (let i = 4; i < process.argv.length; i++) {
+            band += ` ${process.argv[i]}`
+          }
+        } else {
+          band = searchParam;
         }
         axios.get(`${BANDS_URL}${band}/events?app_id=${keys.bands.id}`)
         .then(function (response) {
-          // console.log(response.data);
-          for (let i = 0; i < response.data.length; i++) {
-            let date = moment(response.data[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY")
-            console.log(`${response.data[i].venue.name} - ${response.data[i].venue.city}, ${response.data[i].venue.country} - ${date}\n`)
+          // if band not in api
+          if (response.data.errorMessage !== undefined) {
+            console.log(response.data.errorMessage);
+          } else {
+            for (let i = 0; i < response.data.length; i++) {
+              let date = moment(response.data[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY")              
+              console.log(`${response.data[i].venue.name} - ${response.data[i].venue.city}, ${response.data[i].venue.country} - ${date}\n`)
+            }
           }
         })
         .catch(function (error) {
@@ -34,19 +43,26 @@ function search(userChoice=choice) {
       }
       break;
     case "spotify-this-song":
-      if (process.argv.length > 3) {
-        let song = process.argv[3];
-        for (let i = 4; i < process.argv.length; i++) {
-          song += ` ${process.argv[i]}`
+      if (process.argv.length > 3 || searchParam != undefined) {
+        let song;
+        if (searchParam === undefined) {
+          song = process.argv[3];
+          for (let i = 4; i < process.argv.length; i++) {
+            song += ` ${process.argv[i]}`
+          }
+        } else {
+          song = searchParam;
         }
+        console.log(`56 ${song}`);
         Spotify.search({ type: 'track', query: song }, function(err, data) {
           if (err) {
             return console.log('Error occurred: ' + err);
           }
+          // console.log(data.tracks['items']);
           for (let i = 0; i < data.tracks['items'].length; i++) {
             if (data.tracks['items'][i].name === song) {
               let printThis = data.tracks['items'][i];
-              console.log(`\nSong: ${printThis.artists[0].name}\nArtist: ${printThis.name}\nAlbum: ${printThis.album.name}\n${printThis.href}\n`)
+              console.log(`\nSong: ${printThis.artists[0].name}\nArtist: ${printThis.name}\nAlbum: ${printThis.album.name}\n${printThis.external_urls.spotify}\n`);
             }
           }
         });
@@ -58,30 +74,34 @@ function search(userChoice=choice) {
           }
           for (let i = 0; i < data.tracks['items'].length; i++) {
             if (data.tracks['items'][i].name === 'The Sign') {
-              console.log(data.tracks['items'][i].artists[0].name);
-              console.log(data.tracks['items'][i].name);
-              console.log(data.tracks['items'][i].href);
-              console.log(data.tracks['items'][i].album.name);
+              let printThis = data.tracks['items'][i];
+              console.log(`\nSong: ${printThis.artists[0].name}\nArtist: ${printThis.name}\nAlbum: ${printThis.album.name}\n${printThis.external_urls.spotify}\n`);
             }
           }
         });
       }
       break;
     case "movie-this":
-      if (process.argv.length > 3) {
-        let movie = process.argv[3];
-        for (let i = 4; i < process.argv.length; i++) {
-          movie += ` ${process.argv[i]}`
+      if (process.argv.length > 3 || searchParam != undefined) {
+        let movie;
+        if (searchParam === undefined) {        
+          movie = process.argv[3];
+          for (let i = 4; i < process.argv.length; i++) {
+            movie += ` ${process.argv[i]}`
+          }
+        } else {
+          movie = searchParam
         }
+          
         axios.get(`https://www.omdbapi.com/?t=${movie}&apikey=${keys.movies.id}`)
           .then(function (response) {
-            console.log(response.data['Title']);
-            console.log(response.data['Year']);
-            console.log(response.data['imdbRating']);
-            console.log(response.data['Ratings'][1]['Value']);
-            console.log(response.data['Language']);
-            console.log(response.data['Plot']);
-            console.log(response.data['Actors']);
+            console.log(`\nTitle: ${response.data['Title']}`);
+            console.log(`Released: ${response.data['Year']}`);
+            console.log(`IMDB Rating: ${response.data['imdbRating']}`);
+            console.log(`Rotton Tamatoes Rating: ${response.data['Ratings'][1]['Value']}`);
+            console.log(`Language: ${response.data['Language']}`);
+            console.log(`Plot: ${response.data['Plot']}`);
+            console.log(`Actors: ${response.data['Actors']}\n`);
           })
           .catch(function (error) {
             console.log(error);
@@ -89,13 +109,13 @@ function search(userChoice=choice) {
       } else {
         axios.get(`https://www.omdbapi.com/?t=Mr.+Nobody&apikey=${keys.movies.id}`)
           .then(function (response) {
-            console.log(response.data['Title']);
-            console.log(response.data['Year']);
-            console.log(response.data['imdbRating']);
-            console.log(response.data['Ratings'][1]['Value']);
-            console.log(response.data['Language']);
-            console.log(response.data['Plot']);
-            console.log(response.data['Actors']);
+            console.log(`\nTitle: ${response.data['Title']}`);
+            console.log(`Released: ${response.data['Year']}`);
+            console.log(`IMDB Rating: ${response.data['imdbRating']}`);
+            console.log(`Rotton Tamatoes Rating: ${response.data['Ratings'][1]['Value']}`);
+            console.log(`Language: ${response.data['Language']}`);
+            console.log(`Plot: ${response.data['Plot']}`);
+            console.log(`Actors: ${response.data['Actors']}\n`);
           })
           .catch(function (error) {
             console.log(error);
@@ -113,15 +133,13 @@ function search(userChoice=choice) {
         var dataArr = data.split(",");
         switch(dataArr[0]){
           case('concert-this'):
-            search('concert-this');
+            search('concert-this', dataArr[1]);
             break;
           case('spotify-this-song'):
-            console.log('song!');
-            search('spotify-this-song');
+            search('spotify-this-song', dataArr[1]);
             break;
           case('movie-this'):
-            console.log('movie!');
-            search('movie-this');
+            search('movie-this', dataArr[1]);
             break;
           default:
             console.log
